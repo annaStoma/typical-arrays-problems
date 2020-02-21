5f542faef1c36ca67895db08936878ef2235b83a
Hanna Stoma <annstoma9@gmail.com>
2020-02-21 20:40:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if ( Array.isArray(array) == false || array == undefined)  return 0;
+  else if (array.length == 0) return 0;
+    else {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i])
+                min = array[i];
+        }
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if ( Array.isArray(array) == false || array == undefined)  return 0;
+  else if (array.length == 0) return 0;
+    else {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+        }
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if ( Array.isArray(array) == false || array == undefined)  return 0;
+  else if (array.length == 0) return 0;
+    else {
+        let summ = 0;
+        array.forEach(item => summ += item);
+        console.log(summ / array.length);
+        return summ / array.length;
+    }
 }
